ac9885df3dac850b3e9f1d4e6998a5efa2bdfeb7
Nabeel Saad <nsaad@redhat.com>
2015-10-08 13:30:06 +0100

    Added comments to be clearer

--- MailMessageMover/MailSubjectLabelDelegate.h
+++ MailMessageMover/MailSubjectLabelDelegate.h
@@ -12,8 +12,13 @@
 @interface MailSubjectLabelDelegate : NSObject {
 }
 
+//Label that this delegate handles
 @property (nonatomic, strong) IBOutlet NSTextField * lbStatus;
+
+//error label to show when no messages are selected
 @property (nonatomic, strong) IBOutlet NSTextField * errorLabel;
+
+//access to move button to disable if no messages are selected
 @property (nonatomic, strong) IBOutlet NSButton * moveButton;
 
 - (void) awakeFromNib;
--- MailMessageMover/MailSubjectLabelDelegate.m
+++ MailMessageMover/MailSubjectLabelDelegate.m
@@ -13,16 +13,21 @@
 
 @synthesize lbStatus, errorLabel, moveButton;
 
+//on app start up, run this to check what emails are selected
 - (void) awakeFromNib {
+
     //NSLog(@"in awake from nib in MailSubjectLabelDelegate");
     
     NSString *message = [self updateMessageInfo];
     //NSLog(@"updating label with message: %@", message);
+    //Update the label value to the AppleScript response
     [lbStatus setStringValue:message];
     
+    //Look for text in message
     NSRange range = [message rangeOfString:@"No message selected" options:NSCaseInsensitiveSearch];
     //NSLog(@"rnage location: %lu", range.location);
     
+    //if "No message selected" then set error and disable button
     if (range.location != NSNotFound) {
         [errorLabel setStringValue:@"No messages selected, moving not an option."];
         [moveButton setEnabled:false];
@@ -30,16 +35,21 @@
     
 }
 
+//Calls an AppleScript to get the required details
 - (NSString *) updateMessageInfo {
     NSLog(@"In updateMessageInfo");
     
+    //Get the path of the AppleScript to run
     NSString *path = [[NSBundle mainBundle] pathForResource:@"getMailMessageInfo" ofType:@"scpt"];
+    
+    //Load the AppleScript
     NSAppleScript *script = [[NSAppleScript alloc] initWithContentsOfURL:[NSURL fileURLWithPath:path] error:nil];
     
     if (script != nil) {
+        //Run the AppleScript and get a result
         NSAppleEventDescriptor *result = [script executeAndReturnError:nil];
         NSString *scriptReturn = [result stringValue];
-        NSLog(@"updateMessageInfo script return: %@",scriptReturn);
+        //NSLog(@"updateMessageInfo script return: %@",scriptReturn);
         
         if (scriptReturn == nil)
             return @"N/A";
